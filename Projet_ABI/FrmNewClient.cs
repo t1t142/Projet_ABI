e7b80bd0e6cb9e5b929040555c61b45691cd7010
t1t142 <thierryverrier@orange.fr>
2018-05-03 17:31:49 +0200

    ajout du form new client

---
+++ Projet_ABI/FrmNewClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NouveauxClient
+{
+    public partial class FrmNewClient : Form
+    {
+        public FrmNewClient()
+        {
+            InitializeComponent();
+        }
+
+        private void gbxCaracteristiques_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
